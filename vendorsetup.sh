0908a22bacef7d800f3a4dac08488b87663a3551
Karou <mruczek70@gmail.com>
2021-05-15 20:28:36 +0200

    Davinci: vendorsetup.sh: download hardware caf
    as kang os one doesnt build with davinci, i shipped one from another rom

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,10 @@
 KERNEL=https://github.com/Sleppyiecat/kernel_xiaomi_sm6150
 HW=https://github.com/LineageOS/android_hardware_xiaomi
 VENDOR=https://github.com/Nezuro-Network/vendor_xiaomi_davinci
+CAF=https://github.com/Nezuro-Network/hardware_qcom-caf_sm8150
+
+rm -rf hardware/qcom-caf/sm8150
+git clone $CAF hardware/qcom-caf/sm8150 --depth=1 
 
 if [ ! -d "kernel/xiaomi/sm6150" ]
 then
